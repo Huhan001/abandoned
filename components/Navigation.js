4f32ed11e49311a421b5194396ca4b3fc20ef08f
Humphrey Hanson <114668148+Huhan001@users.noreply.github.com>
2023-09-25 17:13:48 +0200

    project started in next js

---
+++ components/Navigation.js
@@ -0,0 +1,11 @@
+import {Link} from "next/link";
+import { TbArrowBigRightFilled } from 'react-icons/tb';
+import Image from "next/image";
+
+const Navigation = () => {
+    return (
+        <div className='sticky top-0'>
+            
+        </div>
+    )
+}
